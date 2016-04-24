2859ab8a32bd443a65dd7b9a8c96b1c88cb795c2
Nicolas Payot <nicolas.payot@gmail.com>
2016-04-24 17:10:29 +0200

    feat: add ex3 correction

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,8 +5,13 @@ import {Component} from '@angular/core';
   template: `
     <h1>My First {{projectName}}</h1>
     <input [(ngModel)]="projectName">
+    <button (click)="sayHello()">Hello</button>
   `
 })
 export class AppComponent {
   private projectName: string = 'Angular 2 App';
+
+  sayHello() {
+    console.log(`Hello ${this.projectName}!`);
+  }
 }
