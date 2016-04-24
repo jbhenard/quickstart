b2e36856d586b49d1e067766501aeb4f4d485d6e
Nicolas Payot <nicolas.payot@gmail.com>
2016-04-24 16:53:18 +0200

    feat: add ex1 correction

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: '<h1>My First {{projectName}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+  private projectName: string = 'Angular 2 App';
+}
